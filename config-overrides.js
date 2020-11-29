3ff46ce7f97016b02a06196ead7301f90c712a2d
Stefan Devai <stedevai@gmail.com>
2020-11-29 01:24:16 +0100

    add initial react/rust/wasm setup

---
+++ config-overrides.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const { override } = require("customize-cra");
+
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+module.exports = override(
+  // Ignore wasm files on file loader
+  config => {
+    config.module.rules.find(rule => {
+      return (rule.oneOf || []).find(item => {
+        if (item.loader && item.loader.indexOf("file-loader") >= 0) {
+          item.exclude.push(/\.wasm$/);
+          return true;
+        }
+      });
+    });
+
+    return config;
+  },
+
+  // Hook canvas Rust-WASM module
+  config => {
+    config.plugins = (config.plugins || []).concat([
+      new WasmPackPlugin({
+        crateDirectory: path.resolve(__dirname, "./canvas"),
+        extraArgs: "--no-typescript",
+        outDir: path.resolve(__dirname, "./src/canvasWasm")
+      })
+    ]);
+
+    return config;
+  }
+);
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import { greet } from "./canvasWasm";
 
 ReactDOM.render(
   <React.StrictMode>
