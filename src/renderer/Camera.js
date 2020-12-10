9376d5a6bbf84a8c0687e1ed475969b51dabbf26
Stefan Devai <stedevai@gmail.com>
2020-12-10 21:40:37 +0100

    Add zoom towards the center

--- src/renderer/Camera.js
+++ src/renderer/Camera.js
@@ -4,15 +4,17 @@ const ZOOM_MAX = 5.0;
 const ZOOM_MIN = 0.25;
 const ZOOM_THRESHOLD = 10;
 
-const zoomLevels = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3];
+const zoomLevels = [0.25, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75];
 
 class Camera {
   constructor(width, height) {
     this.width = width;
     this.height = height;
     this.zoomLevel = 3;
-    this.zoom = zoomLevels[this.zoomLevel];
-    this.cameraZ = 50.0;
+    this.zoom = 0;
+    this.perceivedWidth = this.width;
+    this.perceivedHeight = this.height;
+    this.cameraZ = 80.0;
     this.position = [0.0, 0.0, this.cameraZ];
     this.near = 0.1;
     this.far = 100.0;
@@ -48,6 +50,9 @@ class Camera {
     else if (this.zoom > 0) {
       this.zoomLevel = Math.min(zoomLevels.length - 1, this.zoomLevel + 1);
     }
+
+    this.perceivedWidth = this.width / zoomLevels[this.zoomLevel];
+    this.perceivedHeight = this.height / zoomLevels[this.zoomLevel];
     this.zoom = 0;
     this.calculateMvp();
   }
@@ -58,28 +63,14 @@ class Camera {
 
   calculateMvp = () => {
     mat4.ortho(this.projection,
-               this.position[0] / zoomLevels[this.zoomLevel],
-               (this.width + this.position[0]) / zoomLevels[this.zoomLevel],
-               (this.height + this.position[1]) / zoomLevels[this.zoomLevel],
-               this.position[1] / zoomLevels[this.zoomLevel],
-               this.near,
-               this.far);
+              (this.width - this.perceivedWidth) + this.position[0] / zoomLevels[this.zoomLevel],
+              this.perceivedWidth + this.position[0] / zoomLevels[this.zoomLevel],
+              this.perceivedHeight + this.position[1] / zoomLevels[this.zoomLevel],
+              (this.height - this.perceivedHeight) + this.position[1] / zoomLevels[this.zoomLevel],
+              this.near,
+              this.far);
 
     mat4.multiply(this.mvp, this.projection, this.modelView);
-  }
-
-  clampZoom = (zoom) => {
-    //let lastDelta = 0.0;
-    //let newLevel = 0.0;
-    //let firstLoop = true;
-
-    //for (const zoomLevel of zoomLevels) {
-      //if (firstLoop) {
-        //lastDelta 
-        //firstLoop = false;
-      //}
-    //}
-    //return Math.min(Math.max(zoom, ZOOM_MIN), ZOOM_MAX);
   }
 };
 
