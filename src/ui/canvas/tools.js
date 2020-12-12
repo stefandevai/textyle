90647a9a14680bfe0606ae9d66d35fcc6a451c8d
Stefan Devai <stedevai@gmail.com>
2020-12-12 17:10:07 +0100

    Simplify src/ui/canvas/EditorCanvas.js code
    - Use drag event to handle continuous tools
    - Remove usingTool state
    - Add Space trigger to pan tool

--- src/ui/canvas/tools.js
+++ src/ui/canvas/tools.js
@@ -2,4 +2,5 @@ export const DEFAULT_TOOL = 'DEFAULT_TOOL';
 export const PLACEMENT_TOOL = 'PLACEMENT_TOOL';
 export const FILL_TOOL = 'FILL_TOOL';
 export const MOVE_TOOL = 'MOVE_TOOL';
+export const PAN_TOOL = 'PAN_TOOL';
 
--- src/ui/hooks.js
+++ src/ui/hooks.js
@@ -22,3 +22,4 @@ export const useEventListener = (eventName, handler, element = window) => {
     [eventName, element]
   );
 };
+
