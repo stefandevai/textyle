23be03aac05651e8c6b7061bd19bc95fda180d97
Stefan Devai <stedevai@gmail.com>
2020-12-21 15:25:35 +0100

    Fix welcome modal bug on firefox

--- src/ui/canvas/EditorCanvas.jsx
+++ src/ui/canvas/EditorCanvas.jsx
@@ -151,9 +151,17 @@ const EditorCanvas = () => {
       }
 
       case tools.PLACEMENT_TOOL: {
-        if (selectedTile !== null && selectedTile !== undefined && selectedTile !== -1) {
-          TilemapInstance.set(...position, selectedTile, layerId);
+        e.preventDefault();
+
+        if (e.button === 2) {
+          TilemapInstance.set(...position, -1, layerId);
         }
+        else if (e.button === 0) {
+          if (selectedTile !== null && selectedTile !== undefined && selectedTile !== -1) {
+            TilemapInstance.set(...position, selectedTile, layerId);
+          }
+        }
+
         break;
       }
 
@@ -219,6 +227,7 @@ const EditorCanvas = () => {
       onMouseDown={handleMouseDown}
       onDragOver={handleDrag}
       onDragStart={handleDragStart}
+      onContextMenu={(e) => e.preventDefault()}
       className="col-span-full row-span-full z-10 w-full h-full"
       draggable="true"
     />
